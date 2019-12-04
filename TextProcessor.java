d0aa57694dfab5bed8bcde539b10ff3b889fa1f7
clgreig <56373629+clgreig@users.noreply.github.com>
2019-12-04 00:05:00 -0700

    Add files via upload

---
+++ TextProcessor.java
@@ -0,0 +1,26 @@
+package teamProject;
+
+import com.sun.javafx.geom.AreaOp.IntOp;
+
+public class TextProcessor
+{
+	private int justification;
+	private int spacing;
+	private int indentation;
+	private int columns;
+	
+	public TextProcessor()
+	{
+		justification = 0; 	//Left justified
+		spacing = 0;		//Single space
+		indentation = 0;	//No indent
+		columns = 0;		//1 column
+	}
+	
+	public String handleText()
+	{
+		
+		return null;
+	}
+	
+}

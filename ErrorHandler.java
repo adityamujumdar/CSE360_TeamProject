8cd45ec4ab33c315a25b8ac9665293fa889be5d3
clgreig <56373629+clgreig@users.noreply.github.com>
2019-12-05 01:11:02 -0700

    Add files via upload

--- ErrorHandler.java
+++ ErrorHandler.java
@@ -1,41 +1,47 @@
-package test;
+package teamProject;
 
-import java.awt.EventQueue;
 import java.util.ArrayList;
-import java.util.Scanner;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
 
-public class ErrorHandler 
+public class ErrorHandler
 {
+	ArrayList<Integer> numError;
+	ArrayList<Integer> lineError;
 	StringBuilder errorString = new StringBuilder();
-	ArrayList<Integer> numError = getErrors();
-	ArrayList<Integer> lineError = getErrorLines();
 	
-	if(!numError.isEmpty)
+	public ErrorHandler(ArrayList<Integer> errorTracker, ArrayList<Integer> errorLines)
 	{
-		for(int i = 0; i < numError.size(); i++)
+		numError = errorTracker;
+		lineError = errorLines;
+	}
+	
+	public StringBuilder processErrors()
+	{
+		if(!numError.isEmpty())
 		{
-			if(numError[i] == 1)
+			for(int i = 0; i < numError.size(); i++)
 			{
-				errorString.append("ERROR 1 ON LINE " + lineError[i] + ": There can not be more than one flag on the same line.");
-				errorString.append("\n");
-			}
-			else if(numError[i] == 2)
-			{
-				errorString.append("ERROR 2 ON LINE " + lineError[i] + ": The flag produced on line " + lineError[i] + " does not exist.");
-				errorString.append("\n");
-			}
-			else if(numError[i] == 3)
-			{
-				errorString.append("ERROR 3: Indentation is not allowed for a column formatted text file.");
-				errorString.append("\n");
+				if(numError.get(i) == 1)
+				{
+					errorString.append("ERROR 1 ON LINE " + lineError.get(i) + ": There can not be more than one flag on the same line.");
+					errorString.append("\n");
+				}
+				else if(numError.get(i) == 2)
+				{
+					errorString.append("ERROR 2 ON LINE " + lineError.get(i) + ": The flag produced on line " + lineError.get(i) + " does not exist.");
+					errorString.append("\n");
+				}
+				else if(numError.get(i) == 3)
+				{
+					errorString.append("ERROR 3 ON LINE " + lineError.get(i) + ": Indentation is not allowed for a column formatted text file.");
+					errorString.append("\n");
+				}
 			}
 		}
-	}
-	else
-	{
-		errorString.append("");
+		else
+		{
+			errorString.append("");
+		}
+		
+		return errorString;
 	}
 }
-
